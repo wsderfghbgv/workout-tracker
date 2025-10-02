ed0ff24aee2897eba0f8f204fb34f784537bd082
andres narvaez <andresdavidnarvaezmejia@gmail.com>
2025-10-01 19:59:07 -0500

    feat: configuraciÃ³n de rutas para nuevos recursos (WorkoutExercise, WorkoutSchedule, WorkoutReport)

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 const userRoutes = require("./user.routes");
 const workoutRoutes = require("./workout.routes");
 const exerciseRoutes = require("./exercise.routes");
+const workoutExerciseRoutes = require("./workout-exercise.routes");
+const workoutScheduleRoutes = require("./workout-schedule.routes");
+const workoutReportRoutes = require("./workout-report.routes");
 
 router.use("/users", userRoutes);
 router.use("/workouts", workoutRoutes);
 router.use("/exercises", exerciseRoutes);
+router.use("/workout-exercises", workoutExerciseRoutes);
+router.use("/workout-schedules", workoutScheduleRoutes);
+router.use("/workout-reports", workoutReportRoutes);
 
 module.exports = router;
