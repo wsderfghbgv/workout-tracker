1fb4844c2c201910c8fe3a4b8a78b900e4a00225
andres narvaez <andresdavidnarvaezmejia@gmail.com>
2025-10-01 19:49:42 -0500

    feat: configuraciÃ³n de rutas principales para todos los recursos

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user.routes");
+const workoutRoutes = require("./workout.routes");
+const exerciseRoutes = require("./exercise.routes");
 
-router.use("/user", userRoutes);
+router.use("/users", userRoutes);
+router.use("/workouts", workoutRoutes);
+router.use("/exercises", exerciseRoutes);
 
 module.exports = router;
